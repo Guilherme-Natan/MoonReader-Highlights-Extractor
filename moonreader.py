dff8d66ad63dc6e384edef80f56ecfdb9f17d0d2
Guilherme Natan <guilherme.natan.braga@gmail.com>
2024-02-24 18:52:02 -0300

    Created the main executable file

---
+++ moonreader.py
@@ -0,0 +1,32 @@
+"""Extracts the content from a mrexpt file (generated from moonreader), 
+and places its contents into a markdown file
+
+Usage: python3 moonreader.py <mrexpt_file_path>
+"""
+
+#!/usr/bin/env python3
+
+import sys
+
+import highlight_sorter
+import markdown_creator
+import mrexpt_parser
+
+
+def main():
+    with open(sys.argv[1], "r", encoding="utf-8") as file:
+        file_content = file.read()
+
+    highlight_list = mrexpt_parser.detect_highlights(file_content)
+    highlight_list_sorted = highlight_sorter.hightlight_sorter(highlight_list)
+    markdown_content = markdown_creator.markdown_creator(highlight_list_sorted)
+
+    filename_markdown_extension = f'{sys.argv[1].split(".")[0]}.md'
+
+    with open(filename_markdown_extension, "w", encoding="utf-8") as file:
+        # Read the entire file content into a variable
+        file.write(markdown_content)
+
+
+if __name__ == "__main__":
+    main()
